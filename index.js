34bd9db23b948133bf2433f0af2a04a201e252aa
Gustavo Merchán <gustavomerchan793@gmail.com>
2025-07-04 21:45:53 -0500

    Limpiar ejemplo

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-    });
-
