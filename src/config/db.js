f13d1f600cf9cf93a1ec7fa70f6856112bded829
rigoloberto <145296949+rigoloberto@users.noreply.github.com>
2025-07-15 11:03:38 -0500

    ssl authoritation for render
    render pide eso tonc le puse

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,10 @@ const pool = new Pool({
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
+    port: process.env.PG_PORT,
+      ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
