41a0c595a666c0fee5ce5fd07d4400fd5d33133b
repozado <joseridriguez65@gmail.com>
2025-07-14 23:34:51 -0500

    auth de mascotas

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const mascotaRoutes = require('./routes/mascota.route');
+const authRoutes = require('./routes/auth.route');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/mascotas', mascotaRoutes);
+app.use('/api/auth', authRoutes);
 module.exports = app;
---
+++ src/routes/auth.route.js
@@ -0,0 +1,8 @@
+// src/routes/auth.route.js
+const express = require('express');
+const router  = express.Router();
+const ctrl    = require('../controllers/auth.controller');
+
+router.post('/login', ctrl.login);
+
+module.exports = router;
