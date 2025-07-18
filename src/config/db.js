887b6f065e34c3266d8b0b3b64c81817934cd08f
rigoloberto <145296949+rigoloberto@users.noreply.github.com>
2025-07-17 21:54:43 -0500

    run online2

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,19 @@
 const {Pool} = require('pg');
 require('dotenv').config();
-
+/*
+ssl authentication
+es requerida para conexion a la bd en la nube
+para correr en localhost no es necesaria
+*/
 const pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-    /*  ssl: {
+    ssl: {
     rejectUnauthorized: false
-  }*/
+  }
 });
 
 module.exports = pool;
