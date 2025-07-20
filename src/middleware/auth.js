7076b0580fcd4706d3738899080ee8b87787b471
rigoloberto <145296949+rigoloberto@users.noreply.github.com>
2025-07-20 18:32:44 -0500

    jeje arreglando
    fundi el sistema como por 15 minutos

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
-const pool = require('../config/db');
 require('dotenv').config();
 
-async function authenticateToken(req, res, next) {
+function authenticateToken(req, res, next) {
   const token = req.header('Authorization')?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
@@ -10,19 +9,10 @@ async function authenticateToken(req, res, next) {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     console.log('Decoded JWT payload:', verified);
-    
-    // Validar si el usuario aún existe
-    const { rows } = await pool.query('SELECT * FROM usuario WHERE id_usuario = $1', [verified.id_usuario]);
-
-    if (rows.length === 0) {
-      return res.status(403).json({ message: 'El usuario ya no existe.' });
-    }
-
     next();
   } catch (error) {
     res.status(401).json({ message: 'Invalid token.' });
   }
-  
 }
 
 module.exports = authenticateToken;
