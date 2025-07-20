60fd795d85e18aac2efaa2cc71c42bb35294d055
repozado <joseridriguez65@gmail.com>
2025-07-20 00:56:14 -0500

    rutas de imagen get all libre para todos

--- src/routes/mascotaImagen.route.js
+++ src/routes/mascotaImagen.route.js
@@ -6,7 +6,7 @@ const controller = require("../controllers/mascotaImagen.controller");
 
 const upload = multer({ storage: multer.memoryStorage() });
 router.post("/:id/imagenes", auth, authorize("admin"), upload.array("imagenes", 10), controller.createMany);
-router.get("/:id/imagenes", auth, controller.getAllByMascota);
+router.get("/:id/imagenes", controller.getAllByMascota);
 router.delete("/:id/imagenes/:imagenId", auth, authorize("admin"), controller.remove);
 
 module.exports = router;
