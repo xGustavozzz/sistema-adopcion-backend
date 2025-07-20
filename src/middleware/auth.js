ace39557c3e7a40ea16c91ea37210582dbf45d12
rigoloberto <145296949+rigoloberto@users.noreply.github.com>
2025-07-20 17:41:19 -0500

    Update auth.js
    para diferenciar token caducado de cuenta borrada

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const pool = require('./db');
 require('dotenv').config();
 
-function authenticateToken(req, res, next) {
+async function authenticateToken(req, res, next) {
   const token = req.header('Authorization')?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
@@ -9,10 +10,19 @@ function authenticateToken(req, res, next) {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     console.log('Decoded JWT payload:', verified);
+    
+    // Validar si el usuario aún existe
+    const { rows } = await pool.query('SELECT * FROM usuario WHERE id_usuario = $1', [verified.id_usuario]);
+
+    if (rows.length === 0) {
+      return res.status(403).json({ message: 'El usuario ya no existe.' });
+    }
+
     next();
   } catch (error) {
     res.status(401).json({ message: 'Invalid token.' });
   }
+  
 }
 
 module.exports = authenticateToken;
